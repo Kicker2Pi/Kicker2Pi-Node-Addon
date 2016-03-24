83996852769e95b61518b1285848c8a4ff285cad
Peter Reuter <piet@difficultdecisions.de>
2016-03-24 10:33:55 +0000

    Add "debounce" code, use pins as pullup due to new wiring

--- kicker2pi.cpp
+++ kicker2pi.cpp
@@ -8,21 +8,52 @@ using std::cout;
 using std::endl;
 using std::flush;
 
-#define LEFT 17;
-#define RIGHT 27;
-#define BUTTON 22;
+#define LEFT 17
+#define RIGHT 27
+#define BUTTON 22
+
+long int lastLeft = 0;
+long int lastRight = 0;
+long int lastButton = 0;
 
 void interrupt_left(void)
 {
-    cout << "Interrupt: left rising to high!" << endl;
+  long int now = millis();
+  if (now - lastLeft > 1000)
+  {
+    lastLeft = now;
+    cout << "Interrupt: left rising to high! Time was " << now << "ms since start." << endl;  
+  }
+  else
+  {
+//    cout << "Bounce Left!" << endl;
+  }
 }
 void interrupt_right(void)
 {
-    cout << "Interrupt: right rising to high!" << endl;
+  long int now = millis();
+  if (now - lastRight > 1000)
+  {
+    lastRight = now;
+    cout << "Interrupt: right rising to high! Time was " << now << "ms since start." << endl;
+  }
+  else
+  {
+//    cout << "Bounce Right!" << endl;
+  }
 }
 void interrupt_button(void)
 {
-    cout << "Interrupt: button rising to high!" << endl;
+  long int now = millis();
+  if (now - lastButton > 1000)
+  {
+    lastButton = now;
+    cout << "Interrupt: button rising to high! Time was " << now << "ms since start." << endl;
+  }
+  else
+  {
+//    cout << "Bounce Button!" << endl;
+  }
 }
 
 
@@ -34,15 +65,15 @@ int main ()
   }
 
   pinMode(LEFT, INPUT);
-  pullUpDnControl(LEFT, PUD_DOWN);
+  pullUpDnControl(LEFT, PUD_UP);
   wiringPiISR(LEFT, INT_EDGE_FALLING, interrupt_left);
 
   pinMode(RIGHT, INPUT);
-  pullUpDnControl(RIGHT, PUD_DOWN);
+  pullUpDnControl(RIGHT, PUD_UP);
   wiringPiISR(RIGHT, INT_EDGE_FALLING, interrupt_right);
 
   pinMode(BUTTON, INPUT);
-  pullUpDnControl(BUTTON, PUD_DOWN);
+  pullUpDnControl(BUTTON, PUD_UP);
   wiringPiISR(BUTTON, INT_EDGE_FALLING, interrupt_button);
 
 
